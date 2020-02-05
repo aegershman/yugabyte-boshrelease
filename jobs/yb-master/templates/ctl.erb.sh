1f1b49ed886ace63b070cab9fb84204b0928608d
Aaron Gershman <aegershman@gmail.com>
2020-02-05 01:22:51 -0600

    stop

--- jobs/yb-master/templates/ctl.erb.sh
+++ jobs/yb-master/templates/ctl.erb.sh
@@ -3,20 +3,22 @@
 set -eu
 
 LOG_DIR=/var/vcap/sys/log/yb-master
-mkdir -p $LOG_DIR
+DATA_DIR=/var/vcap/store/yb-master
+
+mkdir -p $LOG_DIR $DATA_DIR
 exec 1>>"${LOG_DIR}/ctl.stdout.log"
 exec 2>>"${LOG_DIR}/ctl.stderr.log"
 
 case $1 in
 start)
   exec yb-master \
-    --fs_data_dirs=${todo} \
-    --rpc_bind_addresses=${spec_address} \
-    --server_broadcast_addresses=${spec_address}:7100 \
-    --master_addresses=${link} \
-    --replication_factor=${link} \
-    --enable_ysql=${true} \
-    --metric_node_name=$(HOSTNAME) \
+    --fs_data_dirs=${DATA_DIR} \
+    --rpc_bind_addresses={{ spec.address }} \
+    --server_broadcast_addresses={{ spec.address }}:7100 \
+    --master_addresses={{ 'link("yb-master").instances.length' }} \
+    --replication_factor={{ 'link("yb-master").instances.length' }} \
+    --enable_ysql={{ true }} \
+    --metric_node_name={{ HOSTNAME }} \
     --memory_limit_hard_bytes={{ template "yugabyte.memory_hard_limit" $root.Values.resource.master }} \
     --stderrthreshold=0 \
     --num_cpus={{ ceil $root.Values.resource.master.requests.cpu }} \
