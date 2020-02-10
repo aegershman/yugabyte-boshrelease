150811656c7e0aec377ed77d12cb9aaec19b6539
Aaron Gershman <aegershman@gmail.com>
2020-02-10 11:09:36 -0600

    yugabyted

--- jobs/yugabyted/templates/yugabyted_ctl.erb.sh
+++ jobs/yugabyted/templates/yugabyted_ctl.erb.sh
@@ -5,32 +5,33 @@ set -e -u
 RUN_DIR=/var/vcap/sys/run/yugabyted
 LOG_DIR=/var/vcap/sys/log/yugabyted
 STORE_DIR=/var/vcap/store
-DATA_DIR=${STORE_DIR}/yugabyted
-PIDFILE=${RUN_DIR}/pid
+CONFIG_DIR=${STORE_DIR}/yugabyted
+PIDFILE=${RUN_DIR}/yugabyted.pid
 
-mkdir -p ${RUN_DIR} ${LOG_DIR} ${DATA_DIR}
+mkdir -p ${RUN_DIR} ${LOG_DIR} ${CONFIG_DIR} ${DATA_DIR}
 chown -R vcap:vcap ${RUN_DIR} ${LOG_DIR}
 
-exec 1>>"${LOG_DIR}/ctl.stdout.log"
-exec 2>>"${LOG_DIR}/ctl.stderr.log"
+export PIDNUM=$$
+exec > >(tee -a >(logger -p user.info -t vcap.$(basename $0).stdout) | awk -W interactive '{ system("echo -n [$(date +\"%Y-%m-%d %H:%M:%S%z\")] $PIDNUM"); print " " $0 }' >> ${LOG_DIR}/$(basename $0).log)
+exec 2> >(tee -a >(logger -p user.error -t vcap.$(basename $0).stderr) | awk -W interactive '{ system("echo -n [$(date +\"%Y-%m-%d %H:%M:%S%z\")] $PIDNUM"); print " " $0 }' >> ${LOG_DIR}/$(basename $0).err.log)
 
 case $1 in
 start)
-  echo $$ >${PIDFILE}
+  echo ${PIDNUM} >${PIDFILE}
   exec /var/vcap/packages/yugabyte/bin/yugabyted start \
-    --data_dir ${DATA_DIR} \
+    --data_dir ${CONFIG_DIR} \
     --log_dir ${LOG_DIR} \
     --bind_ip <%= spec.address %>
   ;;
 
 stop)
   exec /var/vcap/packages/yugabyte/bin/yugabyted stop
-  kill -9 $(cat ${PIDFILE})
+  kill -9 ${PIDNUM}
   rm -f ${PIDFILE}
   ;;
 
 *)
-  echo "Usage: ${0} {start|stop}"
+  echo "Usage: ${0} {start|stop|status}"
   exit 1
   ;;
 esac
