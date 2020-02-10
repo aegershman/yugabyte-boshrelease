79950430840a610ef3a08f84f8c357a9ee91cc6f
Aaron Gershman <aegershman@gmail.com>
2020-02-10 10:54:53 -0600

    yugabyte ctl erb

--- jobs/yugabyted/templates/yugabyted_ctl.erb.sh
+++ jobs/yugabyted/templates/yugabyted_ctl.erb.sh
@@ -4,28 +4,27 @@ set -e -u
 
 RUN_DIR=/var/vcap/sys/run/yugabyted
 LOG_DIR=/var/vcap/sys/log/yugabyted
-DATA_DIR=/var/vcap/store/yugabyted
+STORE_DIR=/var/vcap/store
+DATA_DIR=${STORE_DIR}/yugabyted
 PIDFILE=${RUN_DIR}/pid
 
 mkdir -p ${RUN_DIR} ${LOG_DIR} ${DATA_DIR}
+chown -R vcap:vcap ${RUN_DIR} ${LOG_DIR}
 
 exec 1>>"${LOG_DIR}/ctl.stdout.log"
 exec 2>>"${LOG_DIR}/ctl.stderr.log"
 
-export PATH=/var/vcap/packages/yugabyte/bin:$PATH
-
 case $1 in
 start)
-
   echo $$ >${PIDFILE}
-
-  exec yugabyted start \
+  exec /var/vcap/packages/yugabyte/bin/yugabyted start \
     --data_dir ${DATA_DIR} \
     --log_dir ${LOG_DIR} \
     --bind_ip <%= spec.address %>
   ;;
 
 stop)
+  exec /var/vcap/packages/yugabyte/bin/yugabyted stop
   kill -9 $(cat ${PIDFILE})
   rm -f ${PIDFILE}
   ;;
