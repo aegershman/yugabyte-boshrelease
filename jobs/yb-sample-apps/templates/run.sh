52b8914cfa68a3a7b64c405d7464c62d92073327
Aaron Gershman <aegershman@gmail.com>
2020-02-11 10:37:51 -0600

    sample apps

--- jobs/yb-sample-apps/templates/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-/var/vcap/jobs/bpm/bin/bpm run yb-sample-apps
---
+++ jobs/yb-sample-apps/templates/yb-sample-apps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+cd /var/vcap/packages/yb-sample-apps
+
+export PATH=$JAVA_HOME/bin:$PATH
+
+java -Xmx${MAX_HEAP_SIZE}m -Djava.security.egd=file:/dev/urandom \
+  -jar *.jar \
+  --workload CassandraKeyValue \
+  --nodes ${ENDPOINTS} \
+  --nouuid \
+  --value_size 256 \
+  --num_threads_read 0 \
+  --num_threads_write 256 \
+  --num_unique_keys 1000000000
