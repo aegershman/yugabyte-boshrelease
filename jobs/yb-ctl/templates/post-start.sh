e57c2cbccb18fcec086649782227433192bf8129
Aaron Gershman <aegershman@gmail.com>
2020-02-13 10:47:53 -0600

    remove ybctl and yugabytedb, move manifests around, cleanup readme

--- jobs/yb-ctl/templates/post-start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-/var/vcap/packages/yugabyte/bin/yb-ctl status
--- jobs/yb-ctl/templates/pre-stop.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-/var/vcap/packages/yugabyte/bin/yb-ctl stop
-/var/vcap/packages/yugabyte/bin/yb-ctl status
