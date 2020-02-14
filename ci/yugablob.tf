0fcbdcede6d3127d959ad1fe84c48d814460c4f8
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-02-13 21:08:36 -0600

    ci/cd first pass (#74)
    * bucket policy
    
    * learning real quick about what others are doing with blobs
    
    * not sure if github actions is the best path forward but its decent as a way to sketch out how this should flow because everybody does it just a littttle bit different
    
    * continuing to just sketch out the overall workflow
    
    * namesquatting real quick
    
    * tf yuga
    
    * give me blobs
    
    * lets sprinkle some region in there and see what happens why not
    
    * remove workflows and scratch task

---
+++ ci/yugablob.tf
@@ -0,0 +1,89 @@
+provider "aws" {
+  version = "~> 2.48"
+  region  = "us-east-2"
+}
+
+####################################
+# blobstore bucket
+####################################
+
+resource "aws_s3_bucket" "yugabyte" {
+  bucket = "yugabyte-boshrelease"
+  acl    = "private"
+
+  tags = {
+    name = "yugabyte-boshrelease"
+    pun  = "yugabucket"
+  }
+}
+
+data "aws_iam_policy_document" "yugabyte_bucket_public_read" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.yugabyte.arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "yugabyte" {
+  bucket = "${aws_s3_bucket.yugabyte.id}"
+  policy = "${data.aws_iam_policy_document.yugabyte_bucket_public_read.json}"
+}
+
+####################################
+# service account for writing to the blobstore
+####################################
+
+resource "aws_iam_user" "yugabyte" {
+  name = "yugabyte"
+
+  tags = {
+    name = "yugabyte-boshrelease"
+    pun  = "yugabot"
+  }
+}
+
+data "aws_iam_policy_document" "yugabyte_blobstore_write" {
+  statement {
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.yugabyte.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "yugabyte_blobstore_write" {
+  name        = "yugabyte-blobstore-write"
+  description = "yugabyte-boshrelease blobstore bucket write permission"
+  policy      = "${data.aws_iam_policy_document.yugabyte_blobstore_write.json}"
+}
+
+resource "aws_iam_user_policy_attachment" "yugabyte" {
+  user       = "${aws_iam_user.yugabyte.name}"
+  policy_arn = "${aws_iam_policy.yugabyte_blobstore_write.arn}"
+}
+
+####################################
+# access key for that service account
+####################################
+
+resource "aws_iam_access_key" "yugabyte" {
+  user = "${aws_iam_user.yugabyte.name}"
+}
+
+output "yugabyte_access_key_id" {
+  value = "${aws_iam_access_key.yugabyte.id}"
+}
+
+output "yugabyte_secret_access_key" {
+  value = "${aws_iam_access_key.yugabyte.encrypted_secret}"
+}
