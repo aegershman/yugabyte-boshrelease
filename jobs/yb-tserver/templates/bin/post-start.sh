898cb8f7138a6a923e009a7cc6b246555d821bc5
Aaron Gershman <aegershman@gmail.com>
2020-02-14 00:23:46 -0600

    needs cleanup and room for improvement but at least I can test more aggressively tomorrow

---
+++ jobs/yb-tserver/templates/bin/post-start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu
+
+source /var/vcap/packages/python-pkg/runtime.env
+
+# as you might expect, TODO
+/var/vcap/packages/yugabyte/bin/cqlsh \
+  --user cassandra \
+  --password cassandra \
+  --debug \
+  --file /var/vcap/jobs/yb-tserver/config/roles.cql \
+  "<%= spec.address %>"
