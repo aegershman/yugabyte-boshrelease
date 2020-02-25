aa5cfa7907f2f65e8c2fa571ad5ad50cc84e074e
Bryan Kelly <910329+xyloman@users.noreply.github.com>
2020-02-25 14:10:35 -0600

    Update pre-start.sh

--- jobs/yb-tserver/templates/bin/pre-start.sh
+++ jobs/yb-tserver/templates/bin/pre-start.sh
@@ -2,4 +2,7 @@
 
 set -eu
 
+# https://docs.yugabyte.com/v1.2/deploy/manual-deployment/system-config/#setting-system-wide-ulimits
+ulimit -l unlimited
+ulimit -a
 /var/vcap/packages/yugabyte/bin/post_install.sh
