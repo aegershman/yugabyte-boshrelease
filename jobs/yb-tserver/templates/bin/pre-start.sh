5d927285811aec8c1c78a5defe23733c410ab8a4
Bryan Kelly <910329+xyloman@users.noreply.github.com>
2020-02-25 14:20:26 -0600

    Update pre-start.sh

--- jobs/yb-tserver/templates/bin/pre-start.sh
+++ jobs/yb-tserver/templates/bin/pre-start.sh
@@ -5,4 +5,7 @@ set -eu
 # https://docs.yugabyte.com/v1.2/deploy/manual-deployment/system-config/#setting-system-wide-ulimits
 ulimit -l unlimited
 ulimit -a
+
+sysctl -w fs.file-max=1048576
+
 /var/vcap/packages/yugabyte/bin/post_install.sh
