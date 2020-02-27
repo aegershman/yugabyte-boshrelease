f2c58359b7519a75d9b4b0b64cc3347ce2e88662
Aaron Gershman <aegershman@gmail.com>
2020-02-27 11:36:37 -0600

    no more privs, test post install location

--- jobs/yb-master/templates/bin/pre-start.sh
+++ jobs/yb-master/templates/bin/pre-start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eux
 
-cd /var/vcap/data/packages/yugabyte/*
+# cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
+cd /var/vcap/packages/yugabyte
 rm -f ".post_install.sh.completed"
 ./bin/post_install.sh
