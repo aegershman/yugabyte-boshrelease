ffb97bed997ae9d663c78992d50fec87d64cf7ad
Aaron Gershman <aegershman@gmail.com>
2020-02-27 10:30:30 -0600

    modified prestart

--- jobs/yb-tserver/templates/bin/pre-start.sh
+++ jobs/yb-tserver/templates/bin/pre-start.sh
@@ -2,17 +2,18 @@
 
 set -eu
 
-# # cd /var/vcap/data/packages/yugabyte/*
-cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
-echo "initial ls"
-ls -la
-echo "attempting rm"
-rm -f ".post_install.sh.completed"
-ls -la
-./bin/post_install.sh
-echo "post-install ls"
-ls -la
-
-# cd /var/vcap/packages/yugabyte
+# /var/vcap/packages/yugabyte/bin/post_install.sh
+# # # cd /var/vcap/data/packages/yugabyte/*
+# cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
+# echo "initial ls"
+# ls -la
+# echo "attempting rm"
 # rm -f ".post_install.sh.completed"
+# ls -la
 # ./bin/post_install.sh
+# echo "post-install ls"
+# ls -la
+
+cd /var/vcap/packages/yugabyte
+# rm -f ".post_install.sh.completed"
+./bin/post_install.sh
