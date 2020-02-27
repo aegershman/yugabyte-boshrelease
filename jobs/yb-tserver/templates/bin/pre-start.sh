d7d889e3ca123958b17c96a3206e8a493b061cb4
Aaron Gershman <aegershman@gmail.com>
2020-02-27 15:19:14 -0600

    commit

--- jobs/yb-tserver/templates/bin/pre-start.sh
+++ jobs/yb-tserver/templates/bin/pre-start.sh
@@ -5,10 +5,11 @@ set -eu
 rm -f /var/vcap/packages/yugabyte/.post_install.sh.completed
 /var/vcap/packages/yugabyte/bin/post_install.sh
 # chown -R root:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapshot
+# chmod 700 /var/vcap/packages/yugabyte/share/
 
-chmod 700 /var/vcap/packages/yugabyte/share/
 chown vcap:vcap -R /var/vcap/data/packages/yugabyte/db77bbcc0b1b255adcfdda76698a1a7051e6b43b
 chown vcap:vcap -R /var/vcap/data/packages/yugabyte/db77bbcc0b1b255adcfdda76698a1a7051e6b43b
+chown vcap:vcap -R /var/vcap/packages/yugabyte
 chown vcap:vcap -R /var/vcap/packages/yugabyte/share
 chown vcap:vcap /var/vcap/data/packages/yugabyte/db77bbcc0b1b255adcfdda76698a1a7051e6b43b/share/initial_sys_catalog_snapshot/exported_tablet_metadata_changes
 chown vcap:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapshot
