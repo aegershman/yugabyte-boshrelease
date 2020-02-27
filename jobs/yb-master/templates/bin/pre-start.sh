f3b9d33bb0d5e8e0b95602f6234ebd1d6f57562b
Aaron Gershman <aegershman@gmail.com>
2020-02-27 11:56:53 -0600

    retest

--- jobs/yb-master/templates/bin/pre-start.sh
+++ jobs/yb-master/templates/bin/pre-start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
-# cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
 cd /var/vcap/packages/yugabyte
 rm -f ".post_install.sh.completed"
 ./bin/post_install.sh
