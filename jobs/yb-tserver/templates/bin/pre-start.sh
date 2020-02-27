0563f9655e437baf78088ced953d50d3435485d0
Aaron Gershman <aegershman@gmail.com>
2020-02-27 10:50:49 -0600

    okay so lets try this one other time, and if this doesnt work we will need to either evaluate capabilities or we will need to reconfigure the bpm startups

--- jobs/yb-tserver/templates/bin/pre-start.sh
+++ jobs/yb-tserver/templates/bin/pre-start.sh
@@ -2,18 +2,6 @@
 
 set -eu
 
-# /var/vcap/packages/yugabyte/bin/post_install.sh
-# # # cd /var/vcap/data/packages/yugabyte/*
-# cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
-# echo "initial ls"
-# ls -la
-# echo "attempting rm"
-# rm -f ".post_install.sh.completed"
-# ls -la
-# ./bin/post_install.sh
-# echo "post-install ls"
-# ls -la
-
-cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
+cd /var/vcap/data/packages/yugabyte/*
 rm -f ".post_install.sh.completed"
 ./bin/post_install.sh
