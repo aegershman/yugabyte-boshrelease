7bd795ab81150b01517384fe0c44af421e9dfa2b
Aaron Gershman <aegershman@gmail.com>
2020-02-27 09:12:30 -0600

    perhaps its just yb pg fallback

--- jobs/yb-tserver/templates/bin/pre-start.sh
+++ jobs/yb-tserver/templates/bin/pre-start.sh
@@ -2,7 +2,11 @@
 
 set -eu
 
-cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
-# cd /var/vcap/data/packages/yugabyte/*
+# cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
+# # cd /var/vcap/data/packages/yugabyte/*
+# rm -f ".post_install.sh.completed"
+# ./bin/post_install.sh
+
+cd /var/vcap/packages/yugabyte
 rm -f ".post_install.sh.completed"
 ./bin/post_install.sh
