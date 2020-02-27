660648b2021bd01a2c1533c7ba63ed9af06505f9
Aaron Gershman <aegershman@gmail.com>
2020-02-26 20:53:42 -0600

    hold up

--- jobs/yb-tserver/templates/bin/pre-start.sh
+++ jobs/yb-tserver/templates/bin/pre-start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
 cd /var/vcap/data/packages/yugabyte/2a0089f71899ee62de9d4b69e69af783745c0881
+# cd /var/vcap/data/packages/yugabyte/*
+rm -f ".post_install.sh.completed"
 ./bin/post_install.sh
