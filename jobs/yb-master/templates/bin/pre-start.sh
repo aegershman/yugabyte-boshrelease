72a2b0a853fe2c1f17a176e12f0dfd2e280ab138
Aaron Gershman <aegershman@gmail.com>
2020-02-27 21:04:40 -0600

    going to replace with ctl to test

--- jobs/yb-master/templates/bin/pre-start.sh
+++ jobs/yb-master/templates/bin/pre-start.sh
@@ -2,8 +2,5 @@
 
 set -eu
 
-rm -f /var/vcap/packages/yugabyte/.post_install.sh.completed
-/var/vcap/packages/yugabyte/bin/post_install.sh
-
-chown vcap:vcap -R /var/vcap/data/packages/yugabyte/db77bbcc0b1b255adcfdda76698a1a7051e6b43b
 chown vcap:vcap -R /var/vcap/packages/yugabyte
+su - vcap -c '/var/vcap/packages/yugabyte/bin/post_install.sh'
