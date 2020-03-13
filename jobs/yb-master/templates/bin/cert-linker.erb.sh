f9d076b377af115175ccc1a0c356da00e9a5cf1d
Aaron Gershman <aegershman@gmail.com>
2020-03-13 12:03:29 -0500

    ysql redux now with some extra hacks and such

--- jobs/yb-master/templates/bin/cert-linker.erb.sh
+++ jobs/yb-master/templates/bin/cert-linker.erb.sh
@@ -33,3 +33,10 @@ set -eu
 #
 ln -sf /var/vcap/jobs/yb-master/config/certs/node.crt /var/vcap/jobs/yb-master/config/certs/node.<%= spec.address %>.crt
 ln -sf /var/vcap/jobs/yb-master/config/certs/node.key /var/vcap/jobs/yb-master/config/certs/node.<%= spec.address %>.key
+
+if [ ! -f "/var/vcap/store/yb-master/pg_data/postgresql.conf" ]; then
+  # initdb creates data directories
+  su - vcap -c "/var/vcap/packages/yugabyte/postgres/bin/initdb --no-locale -D /var/vcap/store/yb-master/pg_data/ -U postgres"
+else
+  echo "appears /var/vcap/store/yb-master/pg_data/postgresql.conf exists and has been initialized"
+fi
--- jobs/yb-tserver/templates/bin/cert-linker.erb.sh
+++ jobs/yb-tserver/templates/bin/cert-linker.erb.sh
@@ -4,3 +4,10 @@ set -eu
 
 ln -sf /var/vcap/jobs/yb-tserver/config/certs/node.crt /var/vcap/jobs/yb-tserver/config/certs/node.<%= spec.address %>.crt
 ln -sf /var/vcap/jobs/yb-tserver/config/certs/node.key /var/vcap/jobs/yb-tserver/config/certs/node.<%= spec.address %>.key
+
+if [ ! -f "/var/vcap/store/yb-tserver/pg_data/postgresql.conf" ]; then
+  # initdb creates data directories
+  su - vcap -c "/var/vcap/packages/yugabyte/postgres/bin/initdb --no-locale -D /var/vcap/store/yb-tserver/pg_data/ -U postgres"
+else
+  echo "appears /var/vcap/store/yb-tserver/pg_data/postgresql.conf exists and has been initialized"
+fi
