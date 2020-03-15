4fd8370590c793221ad5de812909ec820f8a641c
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-03-15 11:43:02 -0500

    YSQL hack scaffolding (#158)
    * move from pre-start to pre_start
    
    * does NOT make ysql viable yet

---
+++ jobs/yb-master/templates/bin/pre_start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eu
+
+/var/vcap/packages/yugabyte/bin/post_install.sh
+/var/vcap/jobs/yb-master/bin/cert-linker.sh
+# /var/vcap/jobs/yb-master/bin/yb-init-pg.sh TODO
---
+++ jobs/yb-master/templates/bin/yb-init-pg.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# if [ ! -f "/var/vcap/store/yb-master/pg_data/postgresql.conf" ]; then
+#   # initdb creates data directories
+#   su - vcap -c "/var/vcap/packages/yugabyte/postgres/bin/initdb --no-locale -D /var/vcap/store/yb-master/pg_data/ -U postgres"
+# else
+#   echo "appears /var/vcap/store/yb-master/pg_data/postgresql.conf exists and has been initialized"
+# fi
