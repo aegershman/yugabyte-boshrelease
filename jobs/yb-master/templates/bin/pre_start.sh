88f2cf9c1d2735ec2c24bdb87115beaa8278e20a
Aaron Gershman <aegershman@gmail.com>
2020-03-15 17:00:54 -0500

    enable ysql manual init now a gated process

--- jobs/yb-master/templates/bin/pre_start.sh
+++ jobs/yb-master/templates/bin/pre_start.sh
@@ -4,4 +4,6 @@ set -eu
 
 /var/vcap/packages/yugabyte/bin/post_install.sh
 /var/vcap/jobs/yb-master/bin/cert-linker.sh
-# /var/vcap/jobs/yb-master/bin/yb-init-pg.sh TODO
+if [ "${ENABLE_MANUAL_YSQL_INIT}" = "true" ]; then
+  /var/vcap/jobs/yb-master/bin/yb-init-pg.sh
+fi
