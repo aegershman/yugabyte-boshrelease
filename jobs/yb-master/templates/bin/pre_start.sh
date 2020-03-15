2ac4b3d3927e1bdd857760517bf6292868ee618f
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-03-15 17:34:57 -0500

    Gate manual running of initdb for YSQL (#162)
    * enable ysql tag
    
    * enable ysql manual init now a gated process
    
    * echo logging saying there wont be a manual init
    
    * smidgeon more logging on what prestart is performing
    
    * enable ysql back to false

--- jobs/yb-master/templates/bin/pre_start.sh
+++ jobs/yb-master/templates/bin/pre_start.sh
@@ -4,4 +4,9 @@ set -eu
 
 /var/vcap/packages/yugabyte/bin/post_install.sh
 /var/vcap/jobs/yb-master/bin/cert-linker.sh
-# /var/vcap/jobs/yb-master/bin/yb-init-pg.sh TODO
+if [ "${ENABLE_MANUAL_YSQL_INIT}" = "true" ]; then
+  echo "pre_start is now performing a manual initdb for ysql"
+  /var/vcap/jobs/yb-master/bin/yb-init-pg.sh
+else
+  echo "pre_start will not be performing a manual initdb for ysql"
+fi
