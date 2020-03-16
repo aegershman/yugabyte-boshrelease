f5639ede10aad5a9c61b3921548ec6bb5606cf1c
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-03-16 17:42:46 -0500

    YSQL snapshot initialization options (#170)
    * remove trailing slash from yb init pg just to keep things tidy
    
    * initializing ysql by having it copy the initial syslog catalog snapshot

--- jobs/yb-tserver/templates/bin/yb-init-pg.sh
+++ jobs/yb-tserver/templates/bin/yb-init-pg.sh
@@ -4,7 +4,7 @@ set -eu
 
 if [ ! -f "/var/vcap/store/yb-tserver/pg_data/postgresql.conf" ]; then
   # initdb creates data directories
-  su - vcap -c "/var/vcap/packages/yugabyte/postgres/bin/initdb --no-locale -D /var/vcap/store/yb-tserver/pg_data/ -U postgres"
+  su - vcap -c "/var/vcap/packages/yugabyte/postgres/bin/initdb --no-locale -D /var/vcap/store/yb-tserver/pg_data -U postgres"
 else
   echo "appears /var/vcap/store/yb-tserver/pg_data/postgresql.conf exists and has been initialized"
 fi
