5dec7e4d8c462500696f05702e72d8b6911d8994
Aaron Gershman <aegershman@gmail.com>
2020-03-16 17:38:05 -0500

    initializing ysql by having it copy the initial syslog catalog snapshot

--- jobs/yb-master/templates/bin/yb-init-pg.sh
+++ jobs/yb-master/templates/bin/yb-init-pg.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+chown -R vcap:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapshot
 if [ ! -f "/var/vcap/store/yb-master/pg_data/postgresql.conf" ]; then
   # initdb creates data directories
   su - vcap -c "/var/vcap/packages/yugabyte/postgres/bin/initdb --no-locale -D /var/vcap/store/yb-master/pg_data -U postgres"
