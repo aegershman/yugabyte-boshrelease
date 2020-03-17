39d56eaf489377232e30a204a5514f8979af8a33
Aaron Gershman <aegershman@gmail.com>
2020-03-17 13:43:59 -0500

    okay maybe not, maybe we have to explicitly pass it, lets find out

--- jobs/yb-master/templates/bin/yb-init-pg.sh
+++ jobs/yb-master/templates/bin/yb-init-pg.sh
@@ -5,7 +5,7 @@ set -eu
 chown -R vcap:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapshot
 if [ ! -f "/var/vcap/store/yb-master/pg_data/postgresql.conf" ]; then
   # initdb creates data directories
-  su - vcap -c '/var/vcap/packages/yugabyte/postgres/bin/initdb -D /var/vcap/store/yb-master/pg_data -U postgres'
+  su - vcap -c 'LC_ALL=C /var/vcap/packages/yugabyte/postgres/bin/initdb -D /var/vcap/store/yb-master/pg_data -U postgres'
 else
   echo "appears /var/vcap/store/yb-master/pg_data/postgresql.conf exists and has been initialized"
 fi
