52404d3ac7b2c517eee5ea51bce2bc5b69d14c3f
Aaron Gershman <aegershman@gmail.com>
2020-03-18 14:37:57 -0500

    ability to rotate the initial seed user of cassandra

--- jobs/yb-tserver/templates/bin/post-deploy.sh
+++ jobs/yb-tserver/templates/bin/post-deploy.sh
@@ -5,6 +5,9 @@ set -eu
 source /var/vcap/packages/python-*/bosh/runtime.env
 
 echo "running post-deploy..."
+
+/var/vcap/jobs/yb-tserver/bin/ycql-admin-pw-cycle.sh
+
 /var/vcap/packages/yugabyte/bin/cqlsh \
   --cqlshrc /var/vcap/jobs/yb-tserver/config/cqlshrc \
   --file /var/vcap/jobs/yb-tserver/config/roles.cql \
