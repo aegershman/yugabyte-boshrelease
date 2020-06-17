e9d1c116da4a248cae5c29b636d99d479405480d
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-06-16 23:01:22 -0500

    move cqlsh references to ycqlsh (#208)

--- jobs/yb-tserver/templates/bin/post-deploy.sh
+++ jobs/yb-tserver/templates/bin/post-deploy.sh
@@ -8,7 +8,7 @@ echo "running post-deploy..."
 
 /var/vcap/jobs/yb-tserver/bin/ycql-rotate-default-admin-password.sh
 
-/var/vcap/packages/yugabyte/bin/cqlsh \
+/var/vcap/packages/yugabyte/bin/ycqlsh \
   --cqlshrc /var/vcap/jobs/yb-tserver/config/cqlshrc \
   --file /var/vcap/jobs/yb-tserver/config/roles.cql \
   --debug
