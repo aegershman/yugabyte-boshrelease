e9f34dd0e9701fdf37f5d04f06d7b93f259cb5fa
Aaron Gershman <aegershman@gmail.com>
2020-07-03 13:31:37 -0500

    rm testdir

--- test/test-post-install-replacement.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-sed '/^distribution_dir=$(cd "$bin_dir/.." && pwd)/a if [[ -L $distribution_dir ]]; then; distribution_dir=$( realpath "$distribution_dir" ); fi' post-install.sh
