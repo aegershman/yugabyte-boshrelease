3c74b75a68842f8a2a9bf07d17c3e28b9ad094a0
Aaron Gershman <aegershman@gmail.com>
2020-07-03 14:44:05 -0500

    handle pre_install

--- jobs/yb-master/templates/bin/pre_start.sh
+++ jobs/yb-master/templates/bin/pre_start.sh
@@ -2,5 +2,8 @@
 
 set -eu
 
+# TODO still necessary?
+chown -R vcap:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapshot
+
 /var/vcap/packages/yugabyte/bin/post_install.sh
 /var/vcap/jobs/yb-master/bin/cert-linker.sh
