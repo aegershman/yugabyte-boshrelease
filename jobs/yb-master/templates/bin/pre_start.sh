18fbccb7b50fbaaaba34a25c73286180f672d798
Aaron Gershman <aegershman@gmail.com>
2020-07-04 15:58:07 -0500

    have rm post start enabled on startup

--- jobs/yb-master/templates/bin/pre_start.sh
+++ jobs/yb-master/templates/bin/pre_start.sh
@@ -5,5 +5,10 @@ set -eu
 # TODO still necessary?
 chown -R vcap:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapshot
 
+RM_POST_INSTALL_COMPLETED_ON_STARTUP=true
+if [ "${RM_POST_INSTALL_COMPLETED_ON_STARTUP}" = "true" ]; then
+  find -L /var/vcap/packages /var/vcap/data/packages -type f -name ".post_install.sh.completed" -delete
+fi
+
 /var/vcap/packages/yugabyte/bin/post_install.sh
 /var/vcap/jobs/yb-master/bin/cert-linker.sh
--- jobs/yb-tserver/templates/bin/pre_start.sh
+++ jobs/yb-tserver/templates/bin/pre_start.sh
@@ -2,5 +2,10 @@
 
 set -eu
 
+RM_POST_INSTALL_COMPLETED_ON_STARTUP=true
+if [ "${RM_POST_INSTALL_COMPLETED_ON_STARTUP}" = "true" ]; then
+  find -L /var/vcap/packages /var/vcap/data/packages -type f -name ".post_install.sh.completed" -delete
+fi
+
 /var/vcap/packages/yugabyte/bin/post_install.sh
 /var/vcap/jobs/yb-tserver/bin/cert-linker.sh
