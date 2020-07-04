609aa23c0e2d9bbe0229dea678b8cea486478926
Aaron Gershman <aegershman@gmail.com>
2020-07-04 16:19:29 -0500

    prestart script go back to using find in limited locations

--- jobs/yb-master/templates/bin/pre_start.sh
+++ jobs/yb-master/templates/bin/pre_start.sh
@@ -6,8 +6,8 @@ set -eu
 chown -R vcap:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapshot
 
 if [ "${RM_POST_INSTALL_COMPLETED_ON_STARTUP}" = "true" ]; then
-  echo "pre_start will be removing the .post_install.sh.completed marker before re-initializing post_install.sh"
-  find -L /var/vcap/ -type f -name ".post_install.sh.completed" -print -delete
+  echo "pre_start will be removing the following .post_install.sh.completed markers before re-initializing post_install.sh..."
+  find -L /var/vcap/packages /var/vcap/data/packages -type f -name ".post_install.sh.completed" -print -delete
 fi
 
 /var/vcap/packages/yugabyte/bin/post_install.sh
