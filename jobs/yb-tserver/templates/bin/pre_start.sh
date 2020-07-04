bb709649accc573682339bbd29bf725d23eb9a64
Aaron Gershman <aegershman@gmail.com>
2020-07-04 16:11:29 -0500

    lets also just do a recursive operation at var vcap root

--- jobs/yb-tserver/templates/bin/pre_start.sh
+++ jobs/yb-tserver/templates/bin/pre_start.sh
@@ -4,7 +4,7 @@ set -eu
 
 if [ "${RM_POST_INSTALL_COMPLETED_ON_STARTUP}" = "true" ]; then
   echo "pre_start will be removing the .post_install.sh.completed marker before re-initializing post_install.sh"
-  find -L /var/vcap/packages /var/vcap/data/packages -type f -name ".post_install.sh.completed" -print -delete
+  find -L /var/vcap/ -type f -name ".post_install.sh.completed" -print -delete
 fi
 
 /var/vcap/packages/yugabyte/bin/post_install.sh
