ea1b72c31e0b6523aa2f0456682971d98b135f9e
Aaron Gershman <aegershman@gmail.com>
2020-07-04 16:10:38 -0500

    also include printing

--- jobs/yb-master/templates/bin/pre_start.sh
+++ jobs/yb-master/templates/bin/pre_start.sh
@@ -7,7 +7,7 @@ chown -R vcap:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapsho
 
 if [ "${RM_POST_INSTALL_COMPLETED_ON_STARTUP}" = "true" ]; then
   echo "pre_start will be removing the .post_install.sh.completed marker before re-initializing post_install.sh"
-  find -L /var/vcap/packages /var/vcap/data/packages -type f -name ".post_install.sh.completed" -delete
+  find -L /var/vcap/packages /var/vcap/data/packages -type f -name ".post_install.sh.completed" -print -delete
 fi
 
 /var/vcap/packages/yugabyte/bin/post_install.sh
