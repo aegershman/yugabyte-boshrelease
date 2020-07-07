637f21cce2b4ead0f58c83239b6ba2b141ee2dd9
Aaron Gershman <aegershman@gmail.com>
2020-07-07 13:23:45 -0500

    readme comment about ysql, fixup initialization scripts

--- jobs/yb-master/templates/bin/pre_start.sh
+++ jobs/yb-master/templates/bin/pre_start.sh
@@ -18,11 +18,12 @@ if [ "${ENABLE_MANUAL_YSQL_INIT}" = "true" ]; then
   YB_ENABLED_IN_POSTGRES=1
   FLAGS_pggate_master_addresses=<%= link("yb-master").instances.map { |i| "#{i.address}:#{p('rpc_bind_addresses_port')}" }.join(",") %>
 
-  if [ -d "/var/vcap/data/yb-master/tmp/pg_data_tmp" ]; then
-    echo "/var/vcap/data/yb-master/tmp/pg_data_tmp appears to exist, will remove before re-initializing..."
+  if [ ! -d "/var/vcap/data/yb-master/tmp/pg_data_tmp" ]; then
+    echo "/var/vcap/data/yb-master/tmp/pg_data_tmp does not appear to exist, now manually initializing with initdb..."
+    su - vcap -c '/var/vcap/packages/yugabyte/postgres/bin/initdb -D /var/vcap/data/yb-master/tmp/pg_data_tmp -U postgres'
+  else
+    echo "/var/vcap/data/yb-master/tmp/pg_data_tmp appears to already exist, not removing anything..."
     echo "WARNING: THIS MEANS YOU SHOULD PROBABLY REMOVE ANY UPGRADE OPSFILES"
-    rm -rf /var/vcap/data/yb-master/tmp/pg_data_tmp
   fi
 
-  su - vcap -c '/var/vcap/packages/yugabyte/postgres/bin/initdb -D /var/vcap/data/yb-master/tmp/pg_data_tmp -U postgres'
 fi
