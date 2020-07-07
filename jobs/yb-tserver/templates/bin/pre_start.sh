22d4110de33d5c69f134282cfa6492123da776e3
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-07-07 13:43:58 -0500

    ysql must be on now (#224)
    * ysql must be on now
    
    * include pre_start to delete the tmp dir
    
    * readme comment about ysql, fixup initialization scripts

--- jobs/yb-tserver/templates/bin/pre_start.sh
+++ jobs/yb-tserver/templates/bin/pre_start.sh
@@ -14,5 +14,13 @@ if [ "${ENABLE_MANUAL_YSQL_INIT}" = "true" ]; then
   echo "pre_start is now performing a manual initdb for ysql"
   YB_ENABLED_IN_POSTGRES=1
   FLAGS_pggate_master_addresses=<%= link("yb-master").instances.map { |i| "#{i.address}:#{p('rpc_bind_addresses_port')}" }.join(",") %>
-  su - vcap -c '/var/vcap/packages/yugabyte/postgres/bin/initdb -D /var/vcap/data/yb-tserver/tmp/pg_data_tmp -U postgres'
+
+  if [ ! -d "/var/vcap/data/yb-tserver/tmp/pg_data_tmp" ]; then
+    echo "/var/vcap/data/yb-tserver/tmp/pg_data_tmp does not appear to exist, now manually initializing with initdb..."
+    su - vcap -c '/var/vcap/packages/yugabyte/postgres/bin/initdb -D /var/vcap/data/yb-tserver/tmp/pg_data_tmp -U postgres'
+  else
+    echo "/var/vcap/data/yb-tserver/tmp/pg_data_tmp appears to already exist, not removing anything..."
+    echo "WARNING: THIS MEANS YOU SHOULD PROBABLY REMOVE ANY UPGRADE OPSFILES"
+  fi
+
 fi
