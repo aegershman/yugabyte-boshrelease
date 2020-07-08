fc74b1a7ff1fbb97b3801e5d5a159a810599e63c
Aaron Gershman <aegershman@gmail.com>
2020-07-08 15:10:55 -0500

    forget the cert linking

--- jobs/yb-tserver/templates/bin/cert-linker.erb.sh
+++ jobs/yb-tserver/templates/bin/cert-linker.erb.sh
@@ -2,6 +2,5 @@
 
 set -eu
 
-chmod 0600 /var/vcap/jobs/yb-tserver/config/certs/node.key
 ln -sf /var/vcap/jobs/yb-tserver/config/certs/node.crt /var/vcap/jobs/yb-tserver/config/certs/node.<%= spec.address %>.crt
 ln -sf /var/vcap/jobs/yb-tserver/config/certs/node.key /var/vcap/jobs/yb-tserver/config/certs/node.<%= spec.address %>.key
