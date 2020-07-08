c62bfdaa830ccf542073119e150613a35be4cd7e
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-07-07 21:32:52 -0500

    ysql basic auth for default yugabyte superuser (#226)
    * ysql basic auth for default yugabyte superuser
    
    * enable ysql auth by default
    
    * account for optional ysql hba conf
    
    * add back default ysql hba conf
    
    * preparing for setting up ysql rotations
    
    * no more special files, only shell scripts now
    
    * export envvars
    
    * roles fixed up
    
    * main manifest
    
    * enable password protection on sqlinserts

--- jobs/yb-tserver/templates/bin/post-deploy.sh
+++ jobs/yb-tserver/templates/bin/post-deploy.sh
@@ -2,13 +2,32 @@
 
 set -eu
 
-source /var/vcap/packages/python*/bosh/runtime.env
-
 echo "running post-deploy..."
 
+source /var/vcap/packages/python*/bosh/runtime.env
+
+echo "running post-deploy ycql rotate admin password check..."
 /var/vcap/jobs/yb-tserver/bin/ycql-rotate-default-admin-password.sh
 
+echo "running post-deploy ycqlsh setup..."
 /var/vcap/packages/yugabyte/bin/ycqlsh \
   --cqlshrc /var/vcap/jobs/yb-tserver/config/cqlshrc \
   --file /var/vcap/jobs/yb-tserver/config/roles.cql \
   --debug
+
+echo "running post-deploy for ysqlsh setup..."
+export PGDATABASE=yugabyte
+export PGHOST=<%= spec.address %>
+export PGPASSWORD=yugabyte
+export PGPORT=<%= p("pgsql_proxy_bind_port") %>
+export PGSSLCERT='/var/vcap/jobs/yb-tserver/config/certs/node.crt'
+export PGSSLKEY='/var/vcap/jobs/yb-tserver/config/certs/node.key'
+export PGSSLMODE='prefer'
+export PGSSLROOTCERT='/var/vcap/jobs/yb-tserver/config/certs/ca.crt'
+export PGUSER=yugabyte
+/var/vcap/packages/yugabyte/bin/ysqlsh \
+  --echo-all \
+  --no-psqlrc \
+  --file=/var/vcap/jobs/yb-tserver/config/roles.sql
+
+echo "post-deploy run complete..."
