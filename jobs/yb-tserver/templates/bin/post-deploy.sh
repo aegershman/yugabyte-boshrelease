09d0623259cf0bd495b401a5bb612df93a3cad97
Aaron Gershman <aegershman@gmail.com>
2020-07-07 20:28:19 -0500

    no more special files, only shell scripts now

--- jobs/yb-tserver/templates/bin/post-deploy.sh
+++ jobs/yb-tserver/templates/bin/post-deploy.sh
@@ -2,19 +2,32 @@
 
 set -eu
 
-source /var/vcap/packages/python*/bosh/runtime.env
-
 echo "running post-deploy..."
 
+source /var/vcap/packages/python*/bosh/runtime.env
+
+echo "running post-deploy ycql rotate admin password check..."
 /var/vcap/jobs/yb-tserver/bin/ycql-rotate-default-admin-password.sh
 
+echo "running post-deploy ycqlsh setup..."
 /var/vcap/packages/yugabyte/bin/ycqlsh \
   --cqlshrc /var/vcap/jobs/yb-tserver/config/cqlshrc \
   --file /var/vcap/jobs/yb-tserver/config/roles.cql \
   --debug
 
-PSQLRC=/var/vcap/jobs/yb-tserver/config/psqlrc
-PGPASSFILE=/var/vcap/jobs/yb-tserver/config/pgpass
+echo "running post-deploy for ysqlsh setup..."
+PGDATABASE=yugabyte
+PGHOST=<%= spec.address %>
+PGPASSWORD=yugabyte
+PGPORT=<%= p("pgsql_proxy_bind_port") %>
+PGSSLCERT='/var/vcap/jobs/yb-tserver/config/certs/node.crt'
+PGSSLKEY='/var/vcap/jobs/yb-tserver/config/certs/node.key'
+PGSSLMODE='prefer'
+PGSSLROOTCERT='/var/vcap/jobs/yb-tserver/config/certs/ca.crt'
+PGUSER=yugabyte
 /var/vcap/packages/yugabyte/bin/ysqlsh \
-  --list
-  # --file=/var/vcap/jobs/yb-tserver/config/roles.sql
+  --echo-all \
+  --no-psqlrc \
+  --file=/var/vcap/jobs/yb-tserver/config/roles.sql
+
+echo "post-deploy run complete..."
