8efaee8d3ce511f35d3379760fcade939be65802
Aaron Gershman <aegershman@gmail.com>
2020-07-07 19:27:55 -0500

    preparing for setting up ysql rotations

--- jobs/yb-tserver/templates/bin/post-deploy.sh
+++ jobs/yb-tserver/templates/bin/post-deploy.sh
@@ -12,3 +12,9 @@ echo "running post-deploy..."
   --cqlshrc /var/vcap/jobs/yb-tserver/config/cqlshrc \
   --file /var/vcap/jobs/yb-tserver/config/roles.cql \
   --debug
+
+PSQLRC=/var/vcap/jobs/yb-tserver/config/psqlrc
+PGPASSFILE=/var/vcap/jobs/yb-tserver/config/pgpass
+/var/vcap/packages/yugabyte/bin/ysqlsh \
+  --list
+  # --file=/var/vcap/jobs/yb-tserver/config/roles.sql
