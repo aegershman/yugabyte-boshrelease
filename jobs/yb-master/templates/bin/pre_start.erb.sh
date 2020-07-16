633c1e4a6fe58a20f035ec75aa10a6c11fd12bd8
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2020-07-16 12:14:54 -0500

    move pre_start to erb, inline erb property resolution (#240)
    * rename sh to have erb for definition
    
    * do the same in yb-master
    
    * heres a happy medium to allow the code to still be rendered but just not ran

--- jobs/yb-master/templates/bin/pre_start.erb.sh
+++ jobs/yb-master/templates/bin/pre_start.erb.sh
@@ -2,10 +2,13 @@
 
 set -eu
 
+chown -R vcap:vcap /var/vcap/packages/yugabyte/share/initial_sys_catalog_snapshot
+
+RM_POST_INSTALL_COMPLETED_ON_STARTUP=<%= p("rm_post_install_completed_on_startup") %>
 if [ "${RM_POST_INSTALL_COMPLETED_ON_STARTUP}" = "true" ]; then
   echo "pre_start will be removing the following .post_install.sh.completed markers before re-initializing post_install.sh..."
   find -L /var/vcap/packages /var/vcap/data/packages -type f -name ".post_install.sh.completed" -print -delete
 fi
 
 /var/vcap/packages/yugabyte/bin/post_install.sh
-/var/vcap/jobs/yb-tserver/bin/cert-linker.sh
+/var/vcap/jobs/yb-master/bin/cert-linker.sh
